cb5fbd090e131844057b1a773e699439545f45b4
anshuman008 <code.anshu@gmail.com>
2025-07-27 23:53:22 +0530

    feat: config added

---
+++ src/config.ts
@@ -0,0 +1,29 @@
+import { Raydium, TxVersion } from '@raydium-io/raydium-sdk-v2'
+import { Connection, Keypair, PublicKey, clusterApiUrl } from '@solana/web3.js'
+
+const HELIUS_RPC=process.env.HTTP_ENDPOINT!;
+
+export const connection = new Connection(HELIUS_RPC) 
+export const txVersion = TxVersion.LEGACY
+const cluster = 'mainnet'
+
+let raydium: Raydium | undefined
+
+export const initSdk = async (wallet: PublicKey, params?: { loadToken?: boolean }) => {
+    if (raydium) return raydium
+
+    if (connection.rpcEndpoint === clusterApiUrl('mainnet-beta')) {// mainnet-beta
+        console.warn('Using free RPC node might cause unexpected errors. Consider using a paid RPC node.')
+    }
+
+    raydium = await Raydium.load({
+        owner: wallet,
+        connection,
+        cluster,
+        disableFeatureCheck: true,
+        disableLoadToken: !params?.loadToken,
+        blockhashCommitment: 'confirmed',
+    })
+
+    return raydium
+}
